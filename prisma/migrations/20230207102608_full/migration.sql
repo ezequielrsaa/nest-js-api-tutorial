f60c240daff89bb80b5b360fdb05fc1c3f54ab7d
ezequielrsaa <ezequielrs_1@hotmail.com>
2023-02-07 08:00:07 -0300

    nestjs crud jwt

---
+++ prisma/migrations/20230207102608_full/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `created_at` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `first_name` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `last_name` on the `users` table. All the data in the column will be lost.
+  - You are about to drop the column `updated_at` on the `users` table. All the data in the column will be lost.
+  - Added the required column `updatedAt` to the `bookmarks` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "bookmarks" DROP CONSTRAINT "bookmarks_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "bookmarks" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" DROP COLUMN "created_at",
+DROP COLUMN "first_name",
+DROP COLUMN "last_name",
+DROP COLUMN "updated_at",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "firstName" TEXT,
+ADD COLUMN     "lastName" TEXT,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "bookmarks" ADD CONSTRAINT "bookmarks_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ src/user/user.service.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { EditUserDto } from "./dto/edit-user.dto";
+import { PrismaService } from "./../prisma/prisma.service";
+import { Injectable } from "@nestjs/common";
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) { }
+  async editUser(userId: number, dto: EditUserDto) {
+    const user = await this.prisma.user.update({
+      where: { id: userId }
+      ,
+      data: {
+        ...dto
+      }
+    });
+    delete user.hash;
+    return user;
+  }
+}
